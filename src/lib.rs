e3bd723ed4a1231eba3c554d734b178004777348
Derek Forgione <derekforgione@gmail.com>
2024-05-24 19:48:10 -0700

    Added ease of use functionality to Value.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod error;
 pub mod parse;
 pub mod format;
 pub use bournemacro::json;
+use error::BourneError;
 
 #[cfg(not(feature = "preserve_order"))]
 pub type ValueMap = std::collections::HashMap<String, Value>;
@@ -58,6 +59,68 @@ impl From<ValueMap> for Value {
 impl From<i64> for Value {
     fn from(value: i64) -> Self {
         Value::Number(value as f64)
+    }
+}
+
+pub trait IndexOrKey {
+    fn get(self, value: &Value) -> Option<&Value>;
+    fn get_mut(self, value: &mut Value) -> Option<&mut Value>;
+}
+
+impl IndexOrKey for usize {
+    fn get(self, value: &Value) -> Option<&Value> {
+        let Value::Array(array) = value else {
+            return None;
+        };
+        array.get(self)
+    }
+
+    fn get_mut(self, value: &mut Value) -> Option<&mut Value> {
+        let Value::Array(array) = value else {
+            return None;
+        };
+        array.get_mut(self)
+    }
+}
+
+impl IndexOrKey for &str {
+    fn get(self, value: &Value) -> Option<&Value> {
+        let Value::Object(object) = value else {
+            return None;
+        };
+        object.get(self)
+    }
+
+    fn get_mut(self, value: &mut Value) -> Option<&mut Value> {
+        let Value::Object(object) = value else {
+            return None;
+        };
+        object.get_mut(self)
+    }
+}
+
+impl Value {
+    pub fn push<T: Into<Value>>(&mut self, value: T) -> Result<(), BourneError> {
+        let Value::Array(array) = self else {    
+            return Err(BourneError::PushToNonArray);
+        };
+        array.push(value.into());
+        Ok(())
+    }
+
+    pub fn insert<T: Into<Value>>(&mut self, k: String, v: T) -> Result<Option<Value>, BourneError> {
+        let Value::Object(object) = self else {
+            return Err(BourneError::InsertToNonObject);
+        };
+        Ok(object.insert(k, v.into()))
+    }
+
+    pub fn get<I: IndexOrKey>(&self, i_k: I) -> Option<&Value> {
+        i_k.get(self)
+    }
+
+    pub fn get_mut<I: IndexOrKey>(&mut self, i_k: I) -> Option<&mut Value> {
+        i_k.get_mut(self)
     }
 }
 
