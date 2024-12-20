a4b70e5d942f4b72f6c7968840ef81dfcd8be245
ErisianArchitect <derekforgione@gmail.com>
2024-12-19 21:48:55 -0800

    Removed unused errors.

--- src/error.rs
+++ src/error.rs
@@ -2,8 +2,6 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum ParseError {
-    #[error("Attempted to step to an invalid fork. Fork must have been created from the parser that you wish to join to.")]
-    StepToInvalidFork,
     #[error("Invalid character at index {0}.")]
     InvalidCharacter(usize),
     #[error("Unexpected end of stream.")]
@@ -18,8 +16,6 @@ pub enum ParseError {
     ParseFloatError(#[from]std::num::ParseFloatError),
     #[error("Invalid escape sequence.")]
     InvalidEscapeSequence,
-    #[error("Invalid Unicode Character.")]
-    InvalidUnicodeCharacter,
     #[error("Invalid Hex.")]
     InvalidHex,
 }
